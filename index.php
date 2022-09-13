70cce43ceb388d9a36709d9b41e828898e15668d
humphreyokoth <humpasp@gmail.com>
2022-09-13 10:20:46 +0300

    Feat:DB connection

--- index.php
+++ index.php
@@ -1,44 +1,44 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Todo</title>
-    <link rel="stylesheet" href="style.css"/>
+    <link rel="stylesheet" href="style.css" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 </head>
 
 <body>
-  <?php require_once("./introphp.php");  
-
+    <?php
+    require_once("./introphp.php");
     include './db_connnection.php';
-
-?>
-<?php   
-  $conn = OpenCon();
-   // echo 'Connect Successfully';
+    ?>
+    <?php
+    $conn = OpenCon();
+    // echo 'Connect Successfully';
     //CloseCon($conn);
-   $sql = " SELECT * FROM to_do_list_items";
+    $sql = " SELECT * FROM to_do_list_items";
     //SELECT * FROM wp_course;
-   $result = $conn->query($sql);
-   if($result-> num_rows>0){
-    echo "id:".$row["id"]."-Title:".$row["title"]."-Date:".$row["data_added"].
-    "<br>";
-   }else{
-    echo "0 results";
-   }
+    $result = $conn->query($sql);
+    if ($result->num_rows > 0) {
+        echo "id:" . $row["id"] . "-Title:" . $row["title"] . "-Date:" . $row["data_added"] .
+            "<br>";
+    } else {
+        echo "0 results";
+    }
 
-   $sql = "INSERT INTO to_do_list_items(title,date_added,type) VALUES ('Todo',now,'type')";
-   //$result = $conn->query($sql);
-   if($result){
-     echo 'Records inserted successfully.';
-   }else {
-     echo 'ERROR:Could not execute $sql.';
-   }
-//    $conn->close();
-  ?>
+    $sql = "INSERT INTO to_do_list_items(title,date_added,type) VALUES ('Todo',now,'type')";
+    //$result = $conn->query($sql);
+    if ($result) {
+        echo 'Records inserted successfully.';
+    } else {
+        echo 'ERROR:Could not execute $sql.';
+    }
+    //    $conn->close();
+    ?>
 
     <div class="container">
         <form action="index.php" method="post" class="addTask" id="addForm" name="todoList">
@@ -70,10 +70,10 @@
             <div class="list" id="list">
 
                 <ul class="list-li" id="task">
-                    
-                <?php  $list = todo();
-                  
-                    foreach($list as $key=> $item) { ?>
+
+                    <?php $list = todo();
+
+                    foreach ($list as $key => $item) { ?>
 
                         <li>
                             <input type="checkbox" name="checkbox" id="list-1" />
